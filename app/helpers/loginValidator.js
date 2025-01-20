5aaa57020e77bb42eb9fbfe1aa5ffcd68b19f094
Levi <139384471+imursugarmommy@users.noreply.github.com>
2025-01-20 13:31:18 +0100

    Update loginValidator.js
    works now you bastard

--- app/helpers/loginValidator.js
+++ app/helpers/loginValidator.js
@@ -1,27 +1,19 @@
 import axios from "axios";
 import { useState } from "react";
 
-
-const [login, setLogin] = useState(false);
-
 export function loginValidator(username, password) {
   const server = "http://192.168.178.60:3000";
-  axios.post(server + "/auth/login", {
-    username: username,
-    password: password,
-  })
+  axios
+    .post(server + "/auth/login", {
+      username: username,
+      password: password,
+    })
     .then((response) => {
-      if (response.data.message == "Erfolgreich eingeloggt.") {
-        console.log("Succes");
-        setLogin(true);
-      } else {
-        console.log("No");
-        setLogin(false);
-      }
+      if (response.data.message == "Erfolgreich eingeloggt.") return true;
+      else return false;
     })
     .catch((error) => {
       console.log(error);
-      setLogin(false);
     });
-    return login;
+  return false;
 }
